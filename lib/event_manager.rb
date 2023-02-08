efadd5085d9bd951911dc41b0826c77b9d22aaf5
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-02-08 18:49:42 -0500

    ADD: refactor clean_zipcode method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,13 +3,7 @@
 require 'csv'
 
 def clean_zipcode(zipcode)
-  if zipcode.nil?
-    zipcode = '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 contents = CSV.open(
