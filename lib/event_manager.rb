4145de900dedd033deca98af835bb0f288a0641f
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-02-10 23:46:11 -0500

    DELETE: commented code and bug creating commas

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,41 +1,16 @@
 # frozen_string_literal: true
 
-require_relative './csv_reader.rb'
-require_relative './zipcode_parser.rb'
-require_relative './legislators_api_getter.rb'
-require_relative './writer.rb'
-require_relative './phone_parser.rb'
-require_relative './registration_analyzer.rb'
+require_relative './csv_reader'
+require_relative './zipcode_parser'
+require_relative './legislators_api_getter'
+require_relative './writer'
+require_relative './phone_parser'
+require_relative './registration_analyzer'
 
 require 'google/apis/civicinfo_v2'
 require 'erb'
 
 puts 'EventManager initialized.'
-
-# contents = CsvReader.new.read('event_attendees.csv')
-# template_letter = File.read('form_letter.erb')
-# erb_template = ERB.new(template_letter)
-# registration_dates = []
-
-# contents.each do |row|
-#   id = row[0]
-#   name = row[:first_name]
-#   phone = PhoneParser.new.parse(row[:homephone])
-#   zipcode = ZipcodeParser.new.parse(row[:zipcode])
-
-#   registration_time = row[:regdate]
-#   registration_dates << registration_time
-
-#   legislators = LegislatorsApiGetter.new.fetch_by_zipcode(zipcode)
-#   form_letter = erb_template.result(binding)
-#   Writer.new.make_thank_you_letter(id, form_letter)
-# end
-
-
-# analyzer = RegistrationAnalyzer.new
-# dates = analyzer.parse_date(registration_dates)
-# analyzer.most_active_day(dates)
-# analyzer.most_active_hour(dates)
 
 class EventManager
 
@@ -47,11 +22,11 @@ class EventManager
     analyzer = RegistrationAnalyzer.new,
     writer = Writer.new
   )
-    @reader = reader,
-    @phone_parser = phone_parser,
-    @zipcode_parser = zipcode_parser,
-    @legislators_getter = legislators_getter,
-    @analyzer = analyzer,
+    @reader = reader
+    @phone_parser = phone_parser
+    @zipcode_parser = zipcode_parser
+    @legislators_getter = legislators_getter
+    @analyzer = analyzer
     @writer = writer
   end
 
@@ -90,7 +65,3 @@ class EventManager
     @analyzer.most_active_hour(dates)
   end
 end
-
-manager = EventManager.new
-manager.generate_thank_you_letter
-manager.highest_registration_day_and_hour
